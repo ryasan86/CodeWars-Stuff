fc0a20ca638f70aa852fff935a4731402d6023d9
Ryan Santos <ryansantos86@gmail.com>
2017-10-05 12:24:42 -0700

    commit

---
+++ findTheUniqueNumber.js
@@ -0,0 +1,12 @@
+function findUniq(arr) {
+  
+    
+
+  return arr;
+}
+
+var example1 = findUniq([1, 1, 1, 2, 1, 1]);
+var example2 = findUniq([0, 0, 0.55, 0, 0]);
+
+console.log('example1->', example1);
+console.log('example2', example2);
