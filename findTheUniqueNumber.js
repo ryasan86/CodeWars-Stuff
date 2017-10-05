d29ca542cbe1b404e9c0dcbcb0a7c939b9eebb9e
Ryan Santos <ryansantos86@gmail.com>
2017-10-05 15:06:57 -0700

    commit

--- findTheUniqueNumber.js
+++ findTheUniqueNumber.js
@@ -1,10 +1,21 @@
+// taken from adrianeyre - doesn't work for decimals
+// https://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
+
 function findUniq(arr) {
-  function onlyUnique(element, index, self) {
-    return self.indexOf(element) === index;
+  nums = {};
+  for (i = 0; i < arr.length; i++) {
+    if (nums[arr[i]] === undefined) {
+      nums[arr[i]] = 1;
+    } else {
+      nums[arr[i]]++;
+    }
   }
-  let unique = arr.filter(onlyUnique);
-  return unique;
+  for (let i in nums) {
+    if (nums[i] === 1) {
+      return parseInt(i);
+    }
+  }
 }
 
-let example1 = findUniq([3, 10, 3, 3, 3]);
+let example1 = findUniq([0, 0, 0.55, 0, 0]);
 console.log(example1);
