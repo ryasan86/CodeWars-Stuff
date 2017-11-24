2158bc2fee97ea0fdc15af3e0ee252a514e69387
Ryan Santos <ryansantos86@gmail.com>
2017-11-23 22:47:05 -0800

    commit

--- 6kyu/consecutive-strings.js
+++ 6kyu/consecutive-strings.js
@@ -1,20 +1,18 @@
-// 6 passed 3 failed
 function longestConsec(strarr, k) {
-  var newarr = [];
   var count = 0;
-  var longest = strarr[0];
+  var difference = strarr.length - k;
 
-  while (count < k) {
+  while (count < difference) {
+    var shortest = strarr.reduce((a, b) => {
+      return a.length < b.length ? a : b;
+    });
 
-    longest = strarr.reduce(function(a, b) {
-      return a.length >= b.length ? a : b;
-    }, '');
+    strarr.splice(strarr.indexOf(shortest), 1);
 
-    newarr.push(longest);
-    strarr.splice(strarr.indexOf(longest), 1);
-    count++;
+    count++;  
   }
-  return newarr.join('');
+
+  return strarr.join('');
 }
 
 function assertEqual(actual, expected) {
@@ -27,16 +25,15 @@ function assertEqual(actual, expected) {
   }
 }
 
-assertEqual(
-  longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2),
-  'abigailtheta'
-);
-assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta");
+assertEqual(longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2), 'abigailtheta');
 assertEqual(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
 assertEqual(longestConsec([], 3), "");
 assertEqual(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
-assertEqual(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu");
-// assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
+assertEqual(
+  longestConsec(['wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu'], 2),
+  'wlwsasphmxxowiaxujylentrklctozmymu'
+);
+assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// 4 passed 5 failed
 function longestConsec(strarr, k) {
-  var newarr = [];
   var count = 0;
-  var longest = strarr[0];
-debugger;
-  while (count < k) {
-    for (var i = 0; i < strarr.length; i++) {
-      if (strarr[i].length > longest.length) {
-        longest = strarr[i];
-        newarr.push(longest);
-      }
-      strarr.splice(strarr.indexOf(longest), 1);
+  var difference = strarr.length - k;
+
+
+  if (strarr.length !== 0 && strarr.length > k && k > 0) {
+    while (count < difference) {
+      var shortest = strarr.reduce((a, b) => {
+        return a.length < b.length ? a : b;
+      });
+  
+      strarr.splice(strarr.indexOf(shortest), 1);
+  
+      count++;  
     }
-    count++;
+  
+    return strarr.join('');
   }
 
-  return newarr.reverse().join('');
+  return '';
 }
 
 function assertEqual(actual, expected) {
@@ -28,16 +30,15 @@ function assertEqual(actual, expected) {
   }
 }
 
-// assertEqual(
-//   longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2),
-//   'abigailtheta'
-// );
-// assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta");
+assertEqual(longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2), 'abigailtheta');
 assertEqual(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
-// assertEqual(longestConsec([], 3), "");
-// assertEqual(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
-// assertEqual(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu");
-// assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
-// assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
+assertEqual(longestConsec([], 3), "");
+assertEqual(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+assertEqual(
+  longestConsec(['wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu'], 2),
+  'wlwsasphmxxowiaxujylentrklctozmymu'
+);
+assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
+assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
