6a2e2439bf4efbe7bbb7e4de5b4abafee20bb57f
Ryan Santos <ryansantos86@gmail.com>
2017-11-25 08:45:42 -0800

    commit

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-function longestConsec(strarr, k) {
-  if (strarr.length > k && k > 0) {
-    strarr = strarr.map(function(el, i, arr) {
-      return arr.slice(i, i + k).join('');
-    });
-
-    return strarr.reduce(function(a, b) {
-      return a.length >= b.length ? a : b;
-    }, '');
+function doneOrNot(board) {
+  for (var i = 0; i < board.length; i++) {
+    
   }
-
-  return '';
 }
 
 function assertEqual(actual, expected) {
@@ -23,17 +15,31 @@ function assertEqual(actual, expected) {
 }
 
 assertEqual(
-  longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2),
-  'abigailtheta'
+  doneOrNot([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]),
+  'Finished!'
 );
-assertEqual(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
-assertEqual(longestConsec([], 3), "");
-assertEqual(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+
 assertEqual(
-  longestConsec(['wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu'], 2),
-  'wlwsasphmxxowiaxujylentrklctozmymu'
+  doneOrNot([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 9],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]),
+  'Try again!'
 );
-assertEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
-assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
-assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
-assertEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
