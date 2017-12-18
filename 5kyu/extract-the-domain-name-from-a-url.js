3cb9308f5379291617deb2b4ff6419ba2c13ca88
Ryan Santos <ryansantos86@gmail.com>
2017-12-18 12:28:07 -0800

    commit

--- 5kyu/extract-the-domain-name-from-a-url.js
+++ 5kyu/extract-the-domain-name-from-a-url.js
@@ -9,12 +9,12 @@ function domainNameBPC(url) {
   return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
 }
 
-function domainNameBPC2(url) {
-  url = url.replace('https://', '');
-  url = url.replace('http://', '');
-  url = url.replace('www.', '');
+function domainNameBPC2(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
   return url.split('.')[0];
-}
+};
 
 function domainName(url) {
   return url
