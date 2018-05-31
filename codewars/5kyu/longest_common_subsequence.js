af2f0f20600f59a6f06a2af5409785886733ec00
Ryan Santos <ryansantos86@gmail.com>
2018-05-30 23:01:58 -0700

    commit

---
+++ codewars/5kyu/longest_common_subsequence.js
@@ -0,0 +1,15 @@
+function LCS(x, y) {
+  let str = '';
+  for (let i = 0; i < y.length; i++) {
+    console.log(x.indexOf(x[i]));
+    console.log(y.indexOf(x[i]));
+    if (x[i] === y[i] || x.indexOf(y[i], i + 1) !== -1) {
+      str += x[i];
+      x = x.substring(x.indexOf(y[i]));
+      console.log(x);
+    }
+  }
+  return str;
+}
+
+console.log(LCS('132535365', '123456789')); // --> "12356"
