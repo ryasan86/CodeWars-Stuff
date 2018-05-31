9d354843f9a6c8ccaf6ebcf10b322e9aa6d253aa
Ryan Santos <ryansantos86@gmail.com>
2018-05-30 17:08:52 -0700

    commit

--- codefights/firstDuplicate.js
+++ codefights/firstDuplicate.js
@@ -1,22 +1,15 @@
 const firstDuplicate = a => {
-    // array for only duplicates found
-    const dupes = [];
-    let result = -1;
+  const obj = {};
 
-    a.forEach((num, idx, arr) => {
-      // search ahead of the array for a duplicate
-      let dupeIdx = arr.indexOf(num, idx + 1);
-      if (dupeIdx !== -1) {
-        // if duplicate found push to dupe array
-        dupes.push([num, dupeIdx]);
-      }
-    });
-    // return dupe with lowest found index
-    if (dupes[0]) {
-      result = dupes.reduce((acc, curr, idx) => (acc[1] < curr[1] ? acc : curr), -1 )[0];
-     }
+  for (let i = 0; i < a.length; i++) {
+    if (obj[a[i]] !== undefined) {
+      return a[i];
+    } else {
+      obj[a[i]] = i;
+    }
+  }
 
-    return result;
-  };
+  return -1;
+};
 
-  console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 2]));
--- codefights/firstNonRepeatingCharacter.js
+++ codefights/firstNonRepeatingCharacter.js
@@ -1,3 +1,19 @@
+/* top solution */
+function firstNotRepeatingCharacter(s) {
+  var arr = s.split('');
+
+  for (var i = 0; i < arr.length; i++) {
+    var chr = arr[i];
+    if (arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])) {
+      return arr[i];
+    }
+  }
+
+  return '_';
+}
+
+
+/* my solution */
 function firstNotRepeatingCharacter(s) {
   for (let i = 0; i < s.length; i++) {
     const currIdx = s.indexOf(s[i]);
