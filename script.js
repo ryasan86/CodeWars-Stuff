3c867d34108f8b6ecbf73b52f5cfc637fe5aac7d
Ryan Santos <ryansantos86@gmail.com>
2018-11-18 10:06:30 -0800

    second branch

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// top solution
-const toUnderScore2 = name => {
-  return name.replace(/(?<=[^_-])_?(?=[A-Z])|(?<=[^\d_])_?(?=\d)/g, '_');
+const addBinary = (a, b) => {
+  const bits = [1, 2, 4, 8, 16, 32, 64, 128].reverse();
+  let sum = a + b;
+
+  return bits.reduce((a, b) => {
+    if (b <= sum) {
+      sum -= b;
+      return a + '1';
+    }
+    return a;
+  }, '');
 };
 
-// my solution
-const toUnderScore = name => {
-  let pattern = /(?<=[^_-])_?([A-Z]|[0-9]+)/g;
-
-  return name.replace(pattern, str => {
-    return `_${str.replace('_', '')}`;
-  });
-};
-
-// console.log(toUnderScore('_If_A_Test_Start_And_Ends_With_Underscore_It_Should_Be_The_Same_'));
-console.log(toUnderScore('Calculate500DividedBy5Equals100'));
-console.log(
-  toUnderScore(
-    '_If_A_Test_Start_And_Ends_With_Underscore_It_Should_Be_The_Same_'
-  )
-);
-
-// console.log('_If'.match(/?<=/));
+console.log(addBinary(51, 12));
