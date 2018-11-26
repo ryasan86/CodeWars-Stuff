36920e6fc0e09d85382fc92d2ae4725cc0c72137
Ryan Santos <ryansantos86@gmail.com>
2018-11-26 10:22:58 -0800

    commit

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const reverse = (str, count = 0) => {
-  const length = str.length - 1;
-  const result = str[length] + str.substr(0, length);
-  return count === str.length ? result : reverse(result, count+=1);
+function isSortedAndHow(array) {
 }
-
-console.log(reverse('abcd'));
