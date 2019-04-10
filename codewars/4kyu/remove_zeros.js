b42f178ad3c847ef2cd202eef67cf4cda40f5df4
Ryan Santos <ryansantos86@gmail.com>
2019-04-10 13:47:15 -0700

    commit

--- codewars/4kyu/remove_zeros.js
+++ codewars/4kyu/remove_zeros.js
@@ -1,16 +1,15 @@
 function removeZeros(array) {
-    let copy = array.slice()
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < copy.length; j++ ) {
-        if (array[i] === 0) {
-          [array[j], copy[i]] = [copy[i], array[j]] 
-        }
+  let copy = array.slice();
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < copy.length; j++) {
+      if (array[i] === 0) {
+        [array[j], copy[i]] = [copy[i], array[j]];
       }
     }
-  
-    return array;
   }
-  
-  var input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14];
-  console.log(removeZeros(input));
-  
+
+  return array;
+}
+
+var input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14];
+console.log(removeZeros(input));
--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-class SequenceSum {
-  static showSequence(n) {
-    return n < 0
-      ? `${n}<0`
-      : `${Array.from({ length: n + 1 }, (v, k) => k).join('+')}${
-          n ? ' = ' : '='
-        }${(n * (n + 1)) / 2}`;
+function pyramid(n){
+  return Array.from({length: n}, (v,i)=>`${' '.repeat(n-i-1)}/${i === n-1 ? '_'.repeat(i * 2) : ' '.repeat(i * 2)}\\`).join('\n') + '\n';
+}
+
+// const pyramid=n=>Array.from({length: n},(a,i)=>" ".repeat(n-i-1)+"/"+(i==n-1 ? "__" : "  ").repeat(i)+"\\\n").join("");
+
+const pyramid = n => {
+  let str = '';
+
+  for (let i = 0; i < n; i++) {
+    const outPadding = ' '.repeat(n - i - 1);
+    const inPadding = (i === n - 1 ? '_' : ' ').repeat(i * 2);
+    str += outPadding + `/${inPadding}\\\n`;
+  }
+
+  return str;
+};
+
+console.log(pyramid(4));
+
+/*
+
+   /\  
+  /  \  
+ /    \ 
+/______\
+
+*/
+
+function countArara(n) {
+  switch (n) {
+    case 0:  return '';
+    case 1:  return 'anane';
+    case 2:  return 'adak';
+    default: return 'adak ' + countArara(n-2);
   }
 }
 
-console.log(SequenceSum.showSequence(0));
-console.log(SequenceSum.showSequence(-1));
+console.log(countArara(3))
